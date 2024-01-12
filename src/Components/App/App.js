4f2cbec4d469ca065b17d46618563b0a07aa6841
Neo <scottfinamore@gmail.com>
2024-01-11 20:02:40 -0500

    Steps 40-55 Adding and removing tracks from Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -37,6 +37,7 @@ class App extends React.Component {
       ],
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
@@ -45,6 +46,13 @@ class App extends React.Component {
       return;
     }
     tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter((currentTrack) => currentTrack.id !== track.id);
+
     this.setState({ playlistTracks: tracks });
   }
 
@@ -64,6 +72,7 @@ class App extends React.Component {
             <Playlist
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}
             />
           </div>
         </div>
